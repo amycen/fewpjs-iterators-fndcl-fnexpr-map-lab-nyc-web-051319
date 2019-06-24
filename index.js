223d4bc4b873a926ba1996fede0294dcead4d7bf
Amy Cen <amy.cen@blackrock.com>
2019-06-24 19:10:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = tutorials.map(tutorial => tutorial.map(word => word.charAt(0).toUpperCase() + word.slice(1)))
+const titleCased = tutorials.map(tutorial => tutorial.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)))
